d74aa4e37cec9dd350e0f7a6503eafb2d625ff5d
Driuk Vladyslav <vlad.dryuk2705@gmail.com>
2024-12-04 10:22:32 +0200

    adding images

---
+++ JS/gallery.js
@@ -0,0 +1,65 @@
+const images = [
+    {
+        preview:
+            'https://nauua.org/media/k2/items/cache/24ca0a85fc075dc4babe60ca9c74b464_XL.jpg',
+        original:
+            'https://photographers.ua/thumbnails/pictures/16048/800xdsc_1637d.jpg',
+        description: ' Замок Любарта ',
+    },
+    {
+        preview:
+            'https://www.worldhistory.org/img/c/p/1600x900/14901.jpg',
+        original:
+            'https://landmarks.in.ua/images/25khotyn-1castle1200x800.jpg',
+        description: ' Хотинська фортеця ',
+    },
+    {
+        preview:
+            'https://lavra.ua/wp-content/uploads/2024/03/photo_2024-03-16_09-59-10-e1710586736540.jpg',
+        original:
+            'https://upload.wikimedia.org/wikipedia/commons/b/b4/%D0%9A%D0%B8%D1%94%D0%B2%D0%BE-%D0%9F%D0%B5%D1%87%D0%B5%D1%80%D1%81%D1%8C%D0%BA%D0%B0_%D0%BB%D0%B0%D0%B2%D1%80%D0%B0._%D0%9A%D0%B8%D1%97%D0%B2.JPG',
+        description: ' Києво-Печерська лавра ',
+    },
+    {
+        preview:
+            'https://18fc33b6ca.cbaul-cdnwnd.com/9c7578031084e7f79abd67a468c3faf9/200000021-8c67f8d5e6/207797_15.jpg',
+        original:
+            'https://upload.wikimedia.org/wikipedia/commons/c/c0/%D0%9B%D0%B0%D1%81%D1%82%D1%96%D0%B2%D1%87%D0%B8%D0%BD%D0%B5_%D0%B3%D0%BD%D1%96%D0%B7%D0%B4%D0%BE._%D0%BC.%D0%AF%D0%BB%D1%82%D0%B0.jpg',
+        description: ' «Ластівчине гніздо» ',
+    },
+    {
+        preview:
+            'https://travels.in.ua/api/Photo/PhotoStreamCPOI/36880',
+        original:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/%D0%91%D1%83%D0%B4%D0%B8%D0%BD%D0%BE%D0%BA_%D1%96%D0%B7_%D1%85%D0%B8%D0%BC%D0%B5%D1%80%D0%B0%D0%BC%D0%B8%2C_%D0%B2_%D1%8F%D0%BA%D0%BE%D0%BC%D1%83_%D0%B6%D0%B8%D0%B2_%D0%B0%D1%80%D1%85%D1%96%D1%82%D0%B5%D0%BA%D1%82%D0%BE%D1%80_%D0%93%D0%BE%D1%80%D0%BE%D0%B4%D0%B5%D1%86%D1%8C%D0%BA%D0%B8%D0%B9.jpg/1200px-%D0%91%D1%83%D0%B4%D0%B8%D0%BD%D0%BE%D0%BA_%D1%96%D0%B7_%D1%85%D0%B8%D0%BC%D0%B5%D1%80%D0%B0%D0%BC%D0%B8%2C_%D0%B2_%D1%8F%D0%BA%D0%BE%D0%BC%D1%83_%D0%B6%D0%B8%D0%B2_%D0%B0%D1%80%D1%85%D1%96%D1%82%D0%B5%D0%BA%D1%82%D0%BE%D1%80_%D0%93%D0%BE%D1%80%D0%BE%D0%B4%D0%B5%D1%86%D1%8C%D0%BA%D0%B8%D0%B9.jpg',
+        description: ' Будинок з химерами ',
+    },
+    {
+        preview:
+            'https://upload.wikimedia.org/wikipedia/commons/6/67/%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B8%D0%B9_%D0%BD%D0%B0%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B8%D0%B9_%D0%B0%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D1%96%D1%87%D0%BD%D0%B8%D0%B9_%D1%82%D0%B5%D0%B0%D1%82%D1%80_%D0%BE%D0%BF%D0%B5%D1%80%D0%B8_%D1%82%D0%B0_%D0%B1%D0%B0%D0%BB%D0%B5%D1%82%D1%83_%D1%96%D0%BC%D0%B5%D0%BD%D1%96_%D0%A1%D0%BE%D0%BB%D0%BE%D0%BC%D1%96%D1%97_%D0%9A%D1%80%D1%83%D1%88%D0%B5%D0%BB%D1%8C%D0%BD%D0%B8%D1%86%D1%8C%D0%BA%D0%BE%D1%97_13.jpg',
+        original:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSutH3bLhfLepd5zHB3TskK_8IdX-LtQTfN6w&s',
+        description: ' Львівська національна опера ',
+    },
+    {
+        preview:
+            'https://www.zoda.gov.ua/images/article/1/000141/141134/picture.jpg',
+        original:
+            'https://ostriv.org/wp-content/uploads/2019/03/DSCN1554-1-%E2%80%94-%D0%BA%D0%BE%D0%BF%D0%B8%D1%8F-e1566499989226.jpg ',
+        description: ' Національний заповідник «Хортиця» ',
+    },
+    {
+        preview:
+            'https://vidviday.ua/blog/wp-content/uploads/2016/01/chernovtsy-tam-ua1.jpg',
+        original:
+            'https://www.chnu.edu.ua/media/mytl0lvp/art_2314.jpg?quality=80&v=1d96c0b26183120',
+        description: ' Чернівецький національний університет ',
+    },
+    {
+        preview:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvQwk3NtsROImJuZ35lJIi5yulnXTsCPUYCQ&s',
+        original:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/%D0%9F%27%D1%8F%D1%82%D0%BD%D0%B8%D1%86%D1%8C%D0%BA%D0%B0_%D1%86%D0%B5%D1%80%D0%BA%D0%B2%D0%B0_%D0%B2%D0%BD%D0%BE%D1%87%D1%96.jpg/2560px-%D0%9F%27%D1%8F%D1%82%D0%BD%D0%B8%D1%86%D1%8C%D0%BA%D0%B0_%D1%86%D0%B5%D1%80%D0%BA%D0%B2%D0%B0_%D0%B2%D0%BD%D0%BE%D1%87%D1%96.jpg',
+        description: ' П`ятницька церква ',
+    }
+]
